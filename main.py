cbbf9e1952d43d0124cb8974fa5676bd8fa4174d
Freaks <galleyhugo@icloud.com>
2024-03-03 12:14:36 +0100

    Correction des bugs Plus amelioration IA

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 
-# Fonction pour vérifier si un joueur a gagné
+# Fonction pour vérifier si un joueur a gagné avec une ligne complète
 def verif_gagne_ligne(grille_jeux):
     for i in range(3):
         premier_element = grille_jeux[i][0]
@@ -12,7 +12,41 @@ def verif_gagne_ligne(grille_jeux):
             return True
     return False
 
+# Fonction pour vérifier si un joueur a gagné avec une colonne complète
+def verif_gagne_colonne(grille_jeux):
+    for i in range(3):
+        premier_element = grille_jeux[0][i]
+        for j in range(1, 3):
+            if grille_jeux[j][i] != premier_element or grille_jeux[j][i] == '':
+                break
+        else:
+            return True
+    return False
 
+# Fonction pour vérifier si un joueur a gagné avec une diagonale complète
+def verif_gagne_diagonale(grille_jeux):
+    premier_element = grille_jeux[0][0]
+    for i in range(1, 3):
+        if grille_jeux[i][i] != premier_element or grille_jeux[i][i] == '':
+            break
+    else:
+        return True
+
+    premier_element = grille_jeux[0][2]
+    for i in range(1, 3):
+        if grille_jeux[i][2 - i] != premier_element or grille_jeux[i][2 - i] == '':
+            break
+    else:
+        return True
+
+# Fonction pour vérifier si la grille est pleine et qu'il n'y a pas de gagnant
+def plein(grille_jeux):
+    for i in range(3):
+        for j in range(3):
+            if grille_jeux[i][j] != '':
+                return False
+    return True
+# Mini_ia pour le joueur 2
 def j2_ia(grille_jeux):
     # Vérification des lignes et des colonnes pour gagner ou bloquer
     for i in range(3):
@@ -49,6 +83,8 @@ def j2_ia(grille_jeux):
         if grille_jeux[1][1] == grille_jeux[2][2] == 'O' and grille_jeux[0][0] == '':
             grille_jeux[0][0] = 'O'
             return grille_jeux
+        if plein(grille_jeux) is True:
+            return grille_jeux
 
     # Si aucune opportunité de gagner ou bloquer, jouer aléatoirement
     while True:
@@ -58,40 +94,6 @@ def j2_ia(grille_jeux):
             grille_jeux[pion_j2_ligne][pion_j2_colonne] = 'O'
             return grille_jeux
 
-
-def verif_gagne_colonne(grille_jeux):
-    for i in range(3):
-        premier_element = grille_jeux[0][i]
-        for j in range(1, 3):
-            if grille_jeux[j][i] != premier_element or grille_jeux[j][i] == '':
-                break
-        else:
-            return True
-    return False
-
-
-def verif_gagne_diagonale(grille_jeux):
-    premier_element = grille_jeux[0][0]
-    for i in range(1, 3):
-        if grille_jeux[i][i] != premier_element or grille_jeux[i][i] == '':
-            break
-    else:
-        return True
-
-    premier_element = grille_jeux[0][2]
-    for i in range(1, 3):
-        if grille_jeux[i][2 - i] != premier_element or grille_jeux[i][2 - i] == '':
-            break
-    else:
-        return True
-
-
-def plein(grille_jeux):
-    for i in range(3):
-        for j in range(3):
-            if grille_jeux[i][j] != '':
-                return False
-    return True
 
 
 # Initialisation de la grille de jeux
@@ -103,7 +105,7 @@ en_jeu = 'y'
 # Boucle de jeu
 
 # Boucle de jeu
-while en_jeu != 'N':
+while en_jeu != 'N' and en_jeu != 'n':
 
     pion_j1_ligne = input('Donnez la ligne : ')
     pion_j1_colonne = input('Donnez la colonne : ')
@@ -121,7 +123,7 @@ while en_jeu != 'N':
         pion_j1_ligne = int(input('Donnez la ligne : '))
         pion_j1_colonne = int(input('Donnez la colonne : '))
 
-    grille_jeux = j2_ia(grille_jeux)
+
 
     # Vérification si la case est déjà occupée pour le joueur
     while grille_jeux[pion_j1_ligne][pion_j1_colonne] != '':
@@ -130,6 +132,9 @@ while en_jeu != 'N':
         pion_j1_colonne = int(input('Donnez la colonne : ')) - 1
     grille_jeux[pion_j1_ligne][pion_j1_colonne] = 'X'
 
+    grille_jeux = j2_ia(grille_jeux)
+    for i in range(3):
+        print(grille_jeux[i])
     # Vérification si un joueur a gagné
     if verif_gagne_ligne(grille_jeux) or verif_gagne_colonne(grille_jeux) or verif_gagne_diagonale(grille_jeux):
         print(f'Félicitations, vous avez gagné !')
@@ -140,5 +145,4 @@ while en_jeu != 'N':
     elif plein(grille_jeux) is True:
         print('Match nul')
 
-    for i in range(3):
-        print(grille_jeux[i])
+
